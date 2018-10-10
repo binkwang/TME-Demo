c12e5c344c7ffd7b8bed5c83c2ac43e485b4e51c
Bink Wang <fxmmc@sina.com>
2018-10-11 00:03:06 +1300

    Avoid incorrect image for the re-used cell

--- TME-Demo/Extention/UIImageView+TME.swift
+++ TME-Demo/Extention/UIImageView+TME.swift
@@ -11,12 +11,16 @@ import Foundation
 
 extension UIImageView {
     func renderImage(imageUrl: String?) {
-        guard let imageUrl = imageUrl, !(imageUrl.isEmpty) else { return }
-        URLSession.shared.dataTask(with: NSURL(string: imageUrl)! as URL) { [weak self] (data, response, error) -> Void in
+        self.image = UIImage(named: "placeholder") // to clear the existing image
+        guard let imageUrl = imageUrl, let url = URL(string: imageUrl) else { return }
+        
+        URLSession.shared.dataTask(with: NSURL(string: imageUrl)! as URL) { [weak self, url] (data, response, error) -> Void in
             guard let strongSelf = self else { return }
-            if let data = data {
-                DispatchQueue.main.async {
-                    strongSelf.image = UIImage(data: data)
+            if let data = data, let responseUrl = response?.url {
+                if url.absoluteString == responseUrl.absoluteString { // to confirmed the corrent image for the cell
+                    DispatchQueue.main.async {
+                        strongSelf.image = UIImage(data: data)
+                    }
                 }
             }
             }.resume()
